7c6a44adeb417a03bf0a1964858242aaaf5eb5b6
Jesus Gonzalez <wilberthg16@gmail.com>
2020-05-12 22:21:38 -0500

    Add version al Css y JS

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,10 +6,10 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>4sea</title>
 
-        <link rel="stylesheet" href="{{asset('css/loading.css')}}">
+        <link rel="stylesheet" href="{{asset('css/loading.css')}}?v=557922">
         <script src="https://kit.fontawesome.com/8ff394cebc.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="{{asset('css/swiper.min.css')}}">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}?v=112489">
     </head>
     <body>
         <div class="loading">
@@ -27,7 +27,7 @@
         @include('include.footer')
 
         <script type="text/javascript" src="{{asset('js/swiper.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/app.js')}}?v=8876628"></script>
 
         @stack('js')
     </body>
